de1b162b9104872638bf100c8bdb956de74620b8
rapidarrow2000@gmail.com <rapidarrow2000@gmail.com>
2014-05-18 16:15:02 +0900

    namespace変更

---
+++ ToDoList/ToDoList/ToDoList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDBC
+{
+    public class ToDoList
+    {
+
+        private List<Todo> _list = new List<Todo>();
+
+        public void Add(Todo todo)
+        {
+            _list.Add(todo);
+        }
+
+
+        public string ViewLastTodo()
+        {
+            return this._list.LastOrDefault().TaskName;
+        }
+
+        public string ViewFirstToDo()
+       {
+           return "最初のTODO";
+        }
+
+        public List<String> ViewAllToDo()
+        {
+            return new List<String>() { 
+                "最初のTODO", "2つめのTODO" 
+            };
+        }
+    }
+}
--- ToDoList/UnitTestProject1/UnitTest1.cs
+++ ToDoList/UnitTestProject1/UnitTest1.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ToDoList;
+using TDDBC;
 
 namespace UnitTestProject1
 {
@@ -10,7 +10,7 @@ namespace UnitTestProject1
         [TestMethod]
         public void ToDoを１つ足してそのToDoが取得できる()
         {
-            var sut = new ToDoList.ToDoList();
+            var sut = new TDDBC.ToDoList();
             sut.Add(new Todo("最初のTODO"));
             sut.ViewLastTodo().Is("最初のTODO");
         }
@@ -18,7 +18,7 @@ namespace UnitTestProject1
         [TestMethod]
         public void ToDoを2つ足して最後のToDoが取得できる()
         {
-            var todoList = new ToDoList.ToDoList();
+            var todoList = new TDDBC.ToDoList();
             todoList.Add(new Todo("最初のTODO"));
             todoList.Add(new Todo("2つめのTODO"));
             todoList.ViewLastTodo().Is("2つめのTODO");
@@ -28,7 +28,7 @@ namespace UnitTestProject1
         [TestMethod]
         public void Todoを2つ足して最初のToDoが取得できる()
         {
-            var sut = new ToDoList.ToDoList();
+            var sut = new TDDBC.ToDoList();
             sut.Add(new Todo("最初のTODO"));
             sut.Add(new Todo("2つめのTODO"));
             sut.ViewFirstToDo().Is("最初のTODO");
@@ -38,7 +38,7 @@ namespace UnitTestProject1
         [TestMethod]
         public void 追加したToDoをすべて見られる()
         {
-            var sut = new ToDoList.ToDoList();
+            var sut = new TDDBC.ToDoList();
             sut.Add(new Todo("最初のTODO"));
             sut.Add(new Todo("2つめのTODO"));
             sut.ViewAllToDo().Is("最初のTODO", "2つめのTODO");
