7c0ef75ad46e97cd5e071c84db54b0282f0799a3
rapidarrow2000@gmail.com <rapidarrow2000@gmail.com>
2014-05-18 14:32:42 +0900

    TODOクラス追加

---
+++ ToDoList/ToDoList/Todo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList
+{
+    public class Todo
+    {
+
+        private string _taskName;
+
+        public Todo(string taskName)
+        {
+            this._taskName = taskName;
+        }
+
+    }
+}
